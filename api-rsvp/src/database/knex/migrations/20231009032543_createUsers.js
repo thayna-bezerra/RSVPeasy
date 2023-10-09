ddc767ac1c42a483c5ad6c1ef978c0843f651497
thayna <arraisthayna2106@gmail.com>
2023-10-09 15:27:53 -0300

    create migration with knex

---
+++ api-rsvp/src/database/knex/migrations/20231009032543_createUsers.js
@@ -0,0 +1,14 @@
+// migrations/create_users_table.js
+exports.up = function (knex) {
+  return knex.schema.createTable('users', function (table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('phone');
+      table.string('email');
+      table.boolean('canSendNotification').defaultTo(true);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
